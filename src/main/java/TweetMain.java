94c6bbe41dcdd5712221a75c73dda180095fbb47
Muhammad Husain <Muhammad Husain>
2015-11-15 20:29:39 +0700

    A. Implemented
    - login
    - mendaftar user baru: insert row ke tabel users
    - follow a friend: insert row ke tabel friends dan followers
    - tweet: insert row ke tabel tweet, userline, timeline dan timeline semua follower
    - menampilkan tweet per user
    - menampilkan timeline per user
    
    B. On going
    
    
    C. Not Yet
    - review code, sesuai gak ama spek?
    - testing

--- src/main/java/TweetMain.java
+++ src/main/java/TweetMain.java
@@ -73,14 +73,14 @@ public class TweetMain {
 
                 }else if ((query = CommandRegexes.EXIT.match(inputs)) != null){
                     System.out.print("Bye....... ");
-                    break;}
-//                }else if ((query = CommandRegexes.TIMELINE.match(inputs)) != null){
-//                    mongo_engine.show_timeline(UserName);
-//                }else if ((query = CommandRegexes.TWEET.match(inputs)) != null){
-//                    mongo_engine.tweet(UserName,query[0]);
-//                }else if ((query = CommandRegexes.MYTWEET.match(inputs)) != null){
-//                    mongo_engine.show_userline(UserName);
-//               }
+                    break;
+                }else if ((query = CommandRegexes.TIMELINE.match(inputs)) != null){
+                    mongo_engine.show_timeline(UserName);
+                }else if ((query = CommandRegexes.TWEET.match(inputs)) != null){
+                    mongo_engine.tweet(UserName,query[0]);
+                }else if ((query = CommandRegexes.MYTWEET.match(inputs)) != null){
+                    mongo_engine.show_userline(UserName);
+               }
                 System.out.print("Query: ");
                 inputs = input.nextLine();
            }
